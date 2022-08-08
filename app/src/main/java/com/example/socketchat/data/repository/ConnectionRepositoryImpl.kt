7aac946c59c681803b7582592d9a4f16f44d9b25
yaronero <yaroslavzakh.prog@gmail.com>
2022-08-08 16:47:09 +0300

    Ping pong fix

--- app/src/main/java/com/example/socketchat/data/repository/ConnectionRepositoryImpl.kt
+++ app/src/main/java/com/example/socketchat/data/repository/ConnectionRepositoryImpl.kt
@@ -7,7 +7,7 @@ import com.example.socketchat.domain.UserSharedPrefsRepository
 import com.google.gson.Gson
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
